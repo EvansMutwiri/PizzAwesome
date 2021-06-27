ee3dff955176c869d1be05b77402420392d3214f
EvansMutwiri <evans.mwiti@student.moringaschool.com>
2021-06-27 10:39:38 +0300

    Add order buttons

---
+++ static/js/main.js
@@ -0,0 +1,5 @@
+function price (size, crust, topping, delivery) {
+    let size = document.querySelector("#size").value;
+    let crust = document.querySelector("#crust").value;
+    topping = document.querySelector("#").value;
+}
